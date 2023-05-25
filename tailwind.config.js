aa233f0f36c691e9385c2cef040d92110b50ca01
Somtoo Chukwu <somtoochukwu@Somtoos-MacBook-Pro-2.local>
2023-05-25 00:14:25 -0400

    Added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'logo':['Sacramento'],
+        'body':['Nunito'],
+        'test':['Monomaniac']
+      }
+      
+    },
   },
   plugins: [],
 }
